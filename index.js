b17ffeeee7ac5dbda37a2e895a129c91703a8081
sibisaravanan1 <sibisaravanan@presidio.com>
2022-03-25 12:54:08 +0530

    add 1

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/1', (req, res) => {
+    res.send('Hello World! 1')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
